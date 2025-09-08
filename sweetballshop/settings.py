e92f04e5695c75c2ca0b91b3a711aceedba04d57
syahwa1105 <qorianasyahwa19@gmail.com>
2025-09-08 21:51:11 +0700

    Tugas Individu 1

--- sweetballshop/settings.py
+++ sweetballshop/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/qoriana.syahwa/sweetballshop"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/qoriana.syahwa/sweetballshop"] #link pws
 
 
 
