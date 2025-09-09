518b809c66bb7498c7df7cbb33daabf230b10d10
syahwa1105 <qorianasyahwa19@gmail.com>
2025-09-09 16:07:09 +0700

    Tugas Individu 2

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class MainViewTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_context_data(self):
+        response = Client().get('')
+        self.assertContains(response, "SweetBall Shop")
+        self.assertContains(response, "2406437533")  
+        self.assertContains(response, "Qoriana Syahwa Maharani")   
+        self.assertContains(response, "PBP-B")
