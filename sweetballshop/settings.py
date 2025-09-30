91558d433a677ec9caa43a13ca9db1220f5d0149
syahwa1105 <qorianasyahwa19@gmail.com>
2025-09-30 14:00:48 +0700

    Tugas Individu 5

--- sweetballshop/settings.py
+++ sweetballshop/settings.py
@@ -144,11 +144,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+if PRODUCTION:
+    # di server PWS → ambil dari collectstatic
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+else:
+    # di local → ambil langsung dari folder static/
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
